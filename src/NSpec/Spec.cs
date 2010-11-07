d95e3d33e566e6169d951c64824d55fc14b13fbb
Shannon Cornish <shannoncornish@gmail.com>
2010-11-07 14:31:27 +1100

    Modified the specify methods to take in expressions

--- src/NSpec/Spec.cs
+++ src/NSpec/Spec.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace NSpec
 {
     public abstract class Spec
     {
-        protected void specify(Action action)
+        protected void specify(Expression<Action> expression)
         {
+            var action = expression.Compile();
             action();
         }
 
-        protected void specify(Func<bool> func)
+        protected void specify(Expression<Func<bool>> expression)
         {
+            var func = expression.Compile();
             var result = func();
             if (!result) 
                 throw new Exception();
